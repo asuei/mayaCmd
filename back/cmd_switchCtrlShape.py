159851419c7057eed182262601535cb7d66549b8
asuei <lingasuei@gmail.com>
2024-12-30 22:39:01 +0800

    Delete cmd_switchCtrlShape.py

--- back/cmd_switchCtrlShape.py
+++
@@ -1,9 +0,0 @@
-sl = cmds.ls(sl=1)
-dpc = cmds.duplicate(sl[0])[0]
-s0 = cmds.listRelatives(dpc,shapes=1)[0]
-s1 = cmds.listRelatives(sl[1],shapes=1)[0]
-cmds.parent(s0,sl[1],r=1,s=1)
-if cmds.objExists('AllSet') :
-    cmds.sets(s0,e=1,addElement='AllSet')
-cmds.delete(s1,dpc)
-cmds.rename(s0,s1)
